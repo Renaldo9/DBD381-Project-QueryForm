8ef3ada8c75e7fc317a7026042ad8623d2987444
Smouskous <mornevanrooyen170@gmail.com>
2025-05-27 16:30:20 +0200

    Added code to allow testing software to work

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const client = new MongoClient(uri);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
